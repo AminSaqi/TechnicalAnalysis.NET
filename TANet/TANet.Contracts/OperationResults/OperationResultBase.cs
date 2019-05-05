4c8f0acc2c2cc509f36c4be9f493ec5473d86c66
AminSaghi <aminsaghi@gmail.com>
2019-05-05 17:48:40 +0430

    Creating OperationResultBase class.

---
+++ TANet/TANet.Contracts/OperationResults/OperationResultBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TANet.Contracts.OperationResults
+{
+    public class OperationResultBase
+    {
+        public bool Success { get; set; }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public static OperationResultBase CreateSuccessResult(string message)
+        {
+            return new OperationResultBase
+            {
+                Success = true,
+                StatusCode = 200,
+                Message = message
+            };
+        }
+
+        public static OperationResultBase CreateExceptionResult(Exception ex)
+        {
+            return new OperationResultBase
+            {
+                Success = false,
+                StatusCode = 500,
+                Message = ex.Message
+            };
+        }
+
+        public static OperationResultBase CreateErrorResult(string errorMessage, int statusCode = 500)
+        {
+            return new OperationResultBase
+            {
+                Success = false,
+                StatusCode = statusCode,
+                Message = errorMessage
+            };
+        }
+    }
+}
