65aad883563ce4c0d51dcb5b6089d93761686e7f
AminSaghi <aminsaghi@gmail.com>
2019-05-05 17:49:01 +0430

    Creating IndicatorResultBase class.

---
+++ TANet/TANet.Contracts/OperationResults/Indicators/IndicatorResultBase.cs
@@ -0,0 +1,10 @@
+﻿
+using TANet.Contracts.Enums;
+
+namespace TANet.Contracts.OperationResults.Indicators
+{
+    public class IndicatorResultBase
+    {
+        public IndicatorSignal IndicatorSignal { get; set; }
+    }
+}
