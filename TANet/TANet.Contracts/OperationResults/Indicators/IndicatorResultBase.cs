69cf25c9515f1c84bad6a0a25fce86bf6337bef2
AminSaghi <aminsaghi@gmail.com>
2019-05-05 17:50:15 +0430

    Adding missed inheritance.

--- TANet/TANet.Contracts/OperationResults/Indicators/IndicatorResultBase.cs
+++ TANet/TANet.Contracts/OperationResults/Indicators/IndicatorResultBase.cs
@@ -3,7 +3,7 @@ using TANet.Contracts.Enums;
 
 namespace TANet.Contracts.OperationResults.Indicators
 {
-    public class IndicatorResultBase
+    public class IndicatorResultBase : OperationResultBase
     {
         public IndicatorSignal IndicatorSignal { get; set; }
     }
