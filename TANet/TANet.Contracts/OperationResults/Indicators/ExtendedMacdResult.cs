c94c76b06b158eaefa4b660d86c848a4a0c75b70
AminSaghi <aminsaghi@gmail.com>
2019-05-05 18:16:16 +0430

    Creating ExtendedMacdResult class.

---
+++ TANet/TANet.Contracts/OperationResults/Indicators/ExtendedMacdResult.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TANet.Contracts.OperationResults.Indicators
+{
+    public class ExtendedMacdResult : IndicatorResultBase
+    {
+        public decimal[] Macd { get; set; }
+        public decimal[] Signal { get; set; }
+        public decimal[] Histogram { get; set; }
+    }
+}
