1d95f1da22f84893239ecda4615c11a24ceae96b
AminSaghi <aminsaghi@gmail.com>
2019-05-19 21:06:02 +0430

    Adding BollingerBandsResult.cs

---
+++ TANet/TANet.Contracts/OperationResults/Indicators/BollingerBandsResult.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TANet.Contracts.OperationResults.Indicators
+{
+    public class BollingerBandsResult : IndicatorResultBase
+    {
+        public decimal[] UpperBand { get; set; }
+        public decimal[] MiddleBand { get; set; }
+        public decimal[] LowerBand { get; set; }
+    }
+}
